77ced203ddc121c850af61efa139d6229c57720c
Lamina <wolfnirte@gmail.com>
2019-07-25 17:11:43 +0200

    added MainMenu

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float PlayerVelocity = 10;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 velocity = PlayerVelocity * new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
+        _rigidbody.velocity = velocity;
+    }
+}
